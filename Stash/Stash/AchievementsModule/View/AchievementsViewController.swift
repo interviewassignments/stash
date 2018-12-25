3abd11830ef9f4e8499c7dd4bceac627d700bf25
Daniel <>
2018-12-25 15:02:20 -0500

    create achievements view controller

--- Stash/Stash/AchievementsModule/View/AchievementsViewController.swift
+++ Stash/Stash/AchievementsModule/View/AchievementsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AchievementsViewController.swift
 //  Stash
 //
 //  Created by Daniel Romero on 12/25/18.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AchievementsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
 
 }
-
